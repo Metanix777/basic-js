742081561387eed361935a8484a1d2645ae5ee7b
Ivan Shukalo <shukalo.96@gmail.com>
2020-02-27 20:50:23 +0300

    feat: add count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,6 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(arr) {
+  let i = 0;
+  arr.map(str => str.map(n => (n == "^^") ? i++ : 0));
+  return i;
+}
+
